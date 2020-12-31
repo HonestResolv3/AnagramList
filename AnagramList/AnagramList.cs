1da06297b7920419ba992732d152bf185691aa8d
Hashim Q <42122270+TruthfullyHonest@users.noreply.github.com>
2020-12-31 12:55:10 -0500

    Add AnagramList V1 Source Files

---
+++ AnagramList/AnagramList.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnagramList
+{
+    public class AnagramList
+    {
+        /**
+         * We do not want this information being accessible outside of it's class, it holds the words the user entered in
+         */
+        private readonly List<string> data;
+
+        /**
+         * Creates a new AnagramList that the user can add words into
+         */
+        public AnagramList()
+        {
+            data = new List<string>();
+        }
+
+        /**
+         * Creates a new AnagramList with elements inside of it
+         */
+        public AnagramList(string[] elements)
+        {
+            data = new List<string>();
+            foreach (string s in elements)
+                data.Add(s);
+        }
+
+        /**
+         * A method to add a new word into the AnagramList
+         */
+        public void Add(string element)
+        {
+            data.Add(element);
+        }
+
+        /**
+         * A method that finds a matching word and any anagrams of that word
+         * An Anagram is defined as "a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman." - Oxford Languages
+         */
+        public List<string> FindMatch(string word)
+        {
+            // Check is there was nothing entered into the AnagramList, return null if so
+            if (data.Count == 0)
+                return null;
+
+            // Iterate through each element of the AnagramList
+            for (int i = 0; i < data.Count; i++)
+            {
+                // Check if there is a match to the exact word the user wants
+                if (data[i].Equals(word))
+                {
+                    // Create a new List that holds strings which will hold matches that we find if they are anagrams and/or the word itself
+                    List<string> matches = new List<string>
+                    {
+
+                        // Add the match into the List
+                        data[i]
+                    };
+
+                    // Turn the match into a character array and sort it
+                    char[] arr = data[i].ToCharArray();
+                    Array.Sort(arr);
+
+                    // Iterate through every element of the collection again (except ignore the index the word was found at first)
+                    for (int j = 0; j < data.Count; j++)
+                    {
+                        // Ignore if the indexes match
+                        if (i == j)
+                            continue;
+
+                        // Create a new character array of the object at index j and sort it
+                        char[] ar = data[j].ToCharArray();
+                        Array.Sort(ar);
+
+                        // We use sorting for both arrays here cause then they both are equal in terms of characters occuring in sequential order
+
+                        // We use System.Linq to check for equality in two arrays, in terms of their content
+                        if (arr.SequenceEqual(ar))
+                            // Add the match if it is an anagram
+                            matches.Add(data[j]);
+                    }
+
+                    // We return ithe new List we created
+                    return matches;
+                }
+            }
+
+            // No matches were found so we return null to represent that
+            return null;
+        }
+    }
+}
---
+++ AnagramList/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AnagramList
+{
+    class Program
+    {
+        /**
+         * We can test out AnagramList here
+         */
+        static void Main()
+        {
+            // This test involves having multiple words that all are anagrams to "mary"
+
+            // .FindMatch(string word) returns a List which has string objects inside it
+            string[] test1 = { "mary", "tea", "java", "goog", "eat", "army", "gary", "ramy", "myra", "mary" };
+            string word1 = "mary";
+
+            // Create a new AnagramList DataStructure that will hold our words
+            AnagramList list1 = new AnagramList(test1);
+
+            // We call the .FindMatch(string word) method and see if there's any matches
+            // Here we are expecting 
+            List<string> matches = list1.FindMatch(word1);
+            Debug.Assert(matches != null);
+            WriteMatches(matches, word1);
+
+
+            // This test involves having no words at all
+            // We repeat the above process again
+            string[] test2 = { };
+            string word2 = "empty";
+
+            AnagramList list2 = new AnagramList(test2);
+
+            matches = list2.FindMatch(word2);
+            Debug.Assert(matches == null);
+            WriteMatches(matches, word2);
+
+            // This test involves having words but there are no matches for part of a word
+            // Similar to the first two, it uses the above process
+            string[] test3 = { "testing", "works", "maybe", "yes", "no" };
+            string word3 = "test";
+
+            AnagramList list3 = new AnagramList(test3);
+
+            matches = list3.FindMatch(word3);
+            Debug.Assert(matches == null);
+            WriteMatches(matches, word3);
+        }
+
+        /**
+         * A method to write the results of the FindMatch() method call
+         */
+        static void WriteMatches(List<string> array, string word)
+        {
+            if (array == null)
+                Console.WriteLine("\n" + word + " was not found in this AnagramList");
+            else
+            {
+                Console.Write("[");
+                foreach (string s in array)
+                    Console.Write(s + ", ");
+                Console.Write("]\n");
+            }
+        }
+    }
+}
